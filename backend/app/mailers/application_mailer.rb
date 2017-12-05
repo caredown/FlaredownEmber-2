7db876356bb83fe4b87e7d77f4353a011522e2cb
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-12-05 13:20:58 +0200

    root_url refactoring in the ApplicationMailer

--- backend/app/mailers/application_mailer.rb
+++ backend/app/mailers/application_mailer.rb
@@ -2,10 +2,8 @@ class ApplicationMailer < ActionMailer::Base
   default from: Rails.application.secrets.smtp_email_from
 
   def client_url(subdomain)
-    secrets = Rails.application.secrets
-
     return if subdomain.empty?
 
-    secrets.base_protocol + subdomain + ".#{secrets.base_domain}"
+    "#{ENV['BASE_PROTOCOL']}#{subdomain}.#{ENV['BASE_DOMAIN']}"
   end
 end
