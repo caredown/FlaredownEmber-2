1e1ddf2acfa0decbca897465a581e4b3c629841c
Kristine.K <hrystyna.kozlovska@litslink.com>
2018-01-16 11:34:35 +0200

    Logo upload without cropping action

--- frontend/app/components/client/client-logo.js
+++ frontend/app/components/client/client-logo.js
@@ -13,37 +13,82 @@ const {
 
 export default imageCropper.extend({
   tagName: 'div',
+  classNames: ['clinet-logo'],
   showPreview: false,
   fileData: null,
 
   aspectRatio: 1,
-  minCropBoxWidth: 100,
-  minCropBoxHeight: 100,
+  minCropBoxWidth: 256,
+  minCropBoxHeight: 256,
+  autoCropArea: 1,
+  zoomOnWheel: false,
   cropperContainer: '.cropper-container > img',
   previewClass: '#logo-preview',
   logo: 'img#clientLogo',
-  croppedAvatar: null,
+  croppedImage: null,
   getCroppedCanvas: 'canvas',
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.$().on('onSaved', () => {
+      if(!get(this, 'isDestroyed')) {
+        set(this, 'showPreview', false);
+        this.$('input[type="file"]').val('');
+      }
+    });
+  },
+
+  getBase64(file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const fileBase64 = reader.result;
+
+      this.sendAction('onCrop', {
+        logo: fileBase64,
+        filename: file.name.split('.')[0],
+        filetype: file.type
+      });
+    };
+  },
 
   actions: {
     logoSelected(file) {
+      this.$(get(this, 'cropperContainer')).cropper('destroy');
       const fileData = URL.createObjectURL(file);
       setProperties(this, { showPreview: true, fileData: fileData, file: file });
-      const reader = new FileReader();
+
+      this.getBase64(file);
     },
 
-    getCroppedAvatar() {
+    getCroppedLogo() {
       const container = this.$(get(this, 'cropperContainer'));
-      const croppedImage = container.cropper('getCroppedCanvas');
+      const croppedCanvas = container.cropper('getCroppedCanvas');
+      const fileData = croppedCanvas.toDataURL();
+      const file = get(this, 'file');
 
-      set(this, 'croppedAvatar', croppedImage);
-    },
-
-    cancelCropp() {
-      const container = this.$(get(this, 'cropperContainer'));
+      set(this, 'fileData', fileData);
       container.cropper('destroy');
 
-      this.sendAction('onLogoSelected', get(this, 'file'));
+      this.sendAction('onCrop', {
+        logo: fileData,
+        filename: file.name.split('.')[0],
+        filetype: file.type
+      });
+    },
+
+    cancelCrop() {
+      const file = get(this, 'file');
+      const container = this.$(get(this, 'cropperContainer'));
+
+      this.getBase64(file);
+
+      const fileData = URL.createObjectURL(file);
+      set(this, 'fileData', fileData);
+
+      container.cropper('destroy');
     },
   },
 });
